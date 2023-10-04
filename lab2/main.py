51ab2aa19b028ac86aebe82f33032d0d244d22aa
shttw <tarbeevaks@mail.ru>
2023-10-04 19:54:31 +0300

    Добавлены комментарии блоков кода

--- lab2/main.py
+++ lab2/main.py
@@ -1,17 +1,20 @@
 import requests
 
+# Инициализация переменных
 s_city = 'Moscow, Ru'
 appid = '8893c7496687e9083f71f231831bbb43'
+
+# Погода сейчас
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
                    params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
-
 print("Город:", s_city)
 print("Погодные условия:", data['weather'][0]['description'])
 print("Температура:", data['main']['temp'])
 print("Минимальная температура:", data['main']['temp_min'])
 print("Максимальная температура", data['main']['temp_max'])
 
+# Расписание погоды на неделю
 res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
                    params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
