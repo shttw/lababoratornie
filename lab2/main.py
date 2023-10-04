8d27032fc10b6e90e9f4b35583801d0baee76d07
vayaka <korotivan2005@yandex.ru>
2023-10-04 15:55:56 +0300

    Добавлена вторая лабораторная

--- lab2/main.py
+++ lab2/main.py
@@ -3,7 +3,7 @@ import requests
 s_city = 'Moscow, Ru'
 appid = '8893c7496687e9083f71f231831bbb43'
 res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPIDЫЫ': appid})
+                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
 
 print("Город:", s_city)
@@ -12,3 +12,12 @@ print("Температура:", data['main']['temp'])
 print("Минимальная температура:", data['main']['temp_min'])
 print("Максимальная температура", data['main']['temp_max'])
 
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                   params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nТемпература <",
+          '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <",
+          i['weather'][0]['description'], ">")
+print("____________________________")
